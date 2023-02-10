e5c1eb0e2591549fa6acabfe7af93222576cfae4
Amber C <amberpomar@gmail.com>
2023-02-10 15:32:20 +0100

    Julio prueba esto

--- test.ts
+++ test.ts
@@ -8,4 +8,11 @@ let ESA = new Movies('El Senyor de los Anillos', 1993, 'EEUU', 'Fantasia');
 let AG = new Movies('los AristoGatos', 1992, 'EEUU', 'Animacion');
 
 let listaPelis = new Imdb([ESA, AG]);
-fs.writeFileSync('BBDD.txt',JSON.stringify(listaPelis));
+fs.writeFileSync('imdbBBDD.json',JSON.stringify(listaPelis));
+
+let text = fs.readFileSync("./BBDD.json")
+let myJsonObject = JSON.parse(text)
+let peliculas2 = new Imdb(myJsonObject)
+console.log(listaPelis);
+console.log(myJsonObject);
+
