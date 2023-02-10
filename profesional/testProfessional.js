68904583d840074a075082a0845bad1c8880ca30
juliop11 <julioperezobrero@hotmail.com>
2023-02-10 12:06:24 +0100

    clase profesional

---
+++ profesional/testProfessional.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var profesional_1 = require("./profesional");
+var datos = new profesional_1.Professional("javi", 1990, 80, 1.80, false, "española", 2, "actor");
+console.log(datos.valorTodos());
---
+++ profesional/testProfessional.ts
@@ -0,0 +1,5 @@
+import { Professional } from "./profesional"
+
+let datos: Professional = new Professional("javi", 1990, 80, 1.80, false, "española", 2, "actor");
+
+console.log(datos.valorTodos());
