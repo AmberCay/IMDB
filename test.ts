96ca278c4a5e75450fecfe31b4a2b4375176cd8e
Amber C <amberpomar@gmail.com>
2023-02-10 13:57:14 +0100

    fs test

--- test.ts
+++ test.ts
@@ -1,7 +1,6 @@
 import { Imdb } from "./Imdb/imdb";
 import { Movies } from "./movies/movies";
-module "File-system" {}
-import * as fs from "./node_modules/file-system"
+import * as fs from "fs";
 
 
 
@@ -9,4 +8,4 @@ let ESA = new Movies('El Senyor de los Anillos', 1993, 'EEUU', 'Fantasia');
 let AG = new Movies('los AristoGatos', 1992, 'EEUU', 'Animacion');
 
 let listaPelis = new Imdb([ESA, AG])
-fs.writeFil
+fs.writeFileSync('BBDD.txt',JSON.stringify(listaPelis))
