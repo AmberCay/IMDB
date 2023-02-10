a55e62bc6a346e804be7e81ec6ccab4eba13ec2b
Amber C <amberpomar@gmail.com>
2023-02-10 14:10:50 +0100

    Fixed fs

--- test.ts
+++ test.ts
@@ -7,5 +7,5 @@ import * as fs from "fs";
 let ESA = new Movies('El Senyor de los Anillos', 1993, 'EEUU', 'Fantasia');
 let AG = new Movies('los AristoGatos', 1992, 'EEUU', 'Animacion');
 
-let listaPelis = new Imdb([ESA, AG])
-fs.writeFileSync('BBDD.txt',JSON.stringify(listaPelis))
+let listaPelis = new Imdb([ESA, AG]);
+fs.writeFileSync('BBDD.txt',JSON.stringify(listaPelis));
