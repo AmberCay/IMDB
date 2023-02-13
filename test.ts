b278f18c2c7beda21de92fbadc72434670f0d1f3
Amber C <amberpomar@gmail.com>
2023-02-13 14:32:31 +0100

    lunes

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import { Imdb } from "./Imdb/imdb";
 import { Movies } from "./movies/movies";
-import * as fs from "fs";
-
+import * as fs from './node_modules';
+import * as path from 'path';
 
 
 let ESA = new Movies('El Senyor de los Anillos', 1993, 'EEUU', 'Fantasia');
@@ -10,7 +10,7 @@ let AG = new Movies('los AristoGatos', 1992, 'EEUU', 'Animacion');
 let listaPelis = new Imdb([ESA, AG]);
 fs.writeFileSync('imdbBBDD.json',JSON.stringify(listaPelis));
 
-let text:string = fs.readFileSync("./BBDD.json")
+let text:string = fs.readFileSync("./imdbBBDD.json", JSON.stringify(listaPelis))
 let myJsonObject = JSON.parse(text)
 
 let movieList:Movies[] = []
